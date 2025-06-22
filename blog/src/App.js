04a88df84f73fce609cd37c55b9326a8bdc091b7
YAMASHITA <yamashita@yamashitakouseinoMacBook-Air.local>
2025-06-23 01:29:34 +0900

    正常に動いた

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter, Route,Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
+import Profile from './pages/Profile';
 
 function App() {
   return(
@@ -12,6 +13,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="signin" element={<Signin />} />
             <Route path="signup" element={<Signup />} />
+            <Route path="/profile/:userId" element={<Profile />} />
           </Routes>
         </div>
       </div>
